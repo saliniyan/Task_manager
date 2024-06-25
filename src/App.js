e17559f17f7293d75c0853cac7ebfdfb08b6cc65
saliniyan <saliniyanp02@gmail.com>
2024-06-25 20:37:59 +0530

    functional component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  const a="hello";
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {/* <p>a</p> */}
-        <p>{a}</p>
-      </header>
-    </div>
-  );
+function App()
+{
+  function random()
+  {
+    const a=["hai","hello","welcome"];
+    const b=Math.floor(Math.random()*3);
+    return a[b];
+  }
+  return(
+    <p>Now displays {random()}</p>
+  )
 }
-
 export default App;
