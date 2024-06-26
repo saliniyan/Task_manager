0b8e83e1caae127fde7935f85e5d8480d4cc262f
saliniyan <saliniyanp02@gmail.com>
2024-06-26 20:32:43 +0530

    list and keys

--- src/Content.js
+++ src/Content.js
@@ -2,36 +2,35 @@ import './App.css'
 import { useState } from 'react';
 function Content()
 {
-  // function random()
-  // {
-  //   const a=["hai","hello","welcome"];
-  //   const b=Math.floor(Math.random()*3);
-  //   return a[b];
-  // }
-  const [count,setcount]=useState(10);
-  const [count2,setcount2]=useState({name2:"hello",textcolor:"blue"});
-  function increment()
-  {
-    setcount((prevcount)=>{return prevcount+1}) //count is just a parameter can give any name
-    setcount(prevcount=> prevcount+1)
-  }
-  function decrement()
-  {
-    setcount(count-1)
-  }
-  function namechange()
-  {
-    setcount2({name2:"changed"})
-    setcount2({textcolor:"red"})
-  }
+    const [item,setitem]=useState([
+      {
+        id:1,
+        value:"wake up",
+        check:true
+      },
+      {
+        id:2,
+        value:"eat",
+        check:true
+      },
+      {
+        id:3,
+        value:"sleep",
+        check:true
+      }
+    ]
+    )
   return(
     <main>
-       <p style={{ color: count2.textcolor }}>Now displays welcome {count2.name2}</p>
-      <button onClick={namechange}>Click here</button>
-      <br />
-      <button onClick={increment}>+</button>
-      <p>{count}</p>
-      <button onClick={decrement}>-</button>
+      <ul>{
+        item.map((i)=>(
+          <li>
+            <input type="checkbox" checked={i.check} />
+            <label>{i.value}</label> 
+          </li>
+        ))
+      }
+      </ul>
     </main>
   )
 }
