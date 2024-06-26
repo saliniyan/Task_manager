7538daf81db014c2edb7aacb0f7e2f62a9c1b40f
saliniyan <saliniyanp02@gmail.com>
2024-06-26 13:17:52 +0530

    tried

--- src/Content.js
+++ src/Content.js
@@ -2,13 +2,14 @@ import './App.css'
 import { useState } from 'react';
 function Content()
 {
-  function random()
-  {
-    const a=["hai","hello","welcome"];
-    const b=Math.floor(Math.random()*3);
-    return a[b];
-  }
+  // function random()
+  // {
+  //   const a=["hai","hello","welcome"];
+  //   const b=Math.floor(Math.random()*3);
+  //   return a[b];
+  // }
   const [count,setcount]=useState(10);
+  const [count2,setcount2]=useState({name2:"hello",textcolor:"blue"});
   function increment()
   {
     setcount((prevcount)=>{return prevcount+1}) //count is just a parameter can give any name
@@ -18,10 +19,15 @@ function Content()
   {
     setcount(count-1)
   }
+  function namechange()
+  {
+    setcount2({name2:"changed"})
+    setcount2({textcolor:"red"})
+  }
   return(
     <main>
-      <p>Now displays welcome</p>
-      <button>Click here</button>
+       <p style={{ color: count2.textcolor }}>Now displays welcome {count2.name2}</p>
+      <button onClick={namechange}>Click here</button>
       <br />
       <button onClick={increment}>+</button>
       <p>{count}</p>
