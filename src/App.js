1fa46e7752fb14f1597e4965a1a1f7c9652f9655
saliniyan <saliniyanp02@gmail.com>
2024-07-11 18:58:23 +0530

    starting new

--- src/App.js
+++ src/App.js
@@ -1,41 +1,25 @@
-import Content from './Content';
-import Head from './Header';
-import Footer from './Footer';
-import './App.css'
-import { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-
-function App()
-{
-  const [item,setitem]=useState([
-    {
-      id:1,
-      value:"wake up",
-      check:true
-    },
-    {
-      id:2,
-      value:"eat",
-      check:true
-    },
-    {
-      id:3,
-      value:"sleep",
-      check:true
-    }
-  ]
-  )
-  return(
-    <div className="container">
-    <Head title="This is heading "/>
-    <Content 
-      item={item}
-      // setitem={setitem} no need to send it because we will also send the function '''not here''' that will take care
-    />
-    <Footer 
-      len={item.length}
-    />
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
-export default App;   
+
+export default App;
